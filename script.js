648deefa9d52234f574f5ac80dbe8636b3dbe70a
AR-Nasim <a.r.nasim5250@gmail.com>
2021-07-08 23:46:11 +0600

    Added javaScript

---
+++ script.js
@@ -0,0 +1,22 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const mins = document.getElementById('minutes');
+const secs = document.getElementById('seconds');
+
+function timer(){
+    const currentDate = new Date();
+    const offerTime = new Date("31 July 2021");
+    const totalSec = (offerTime-currentDate)/1000;
+    
+    const remainingDays = Math.floor(totalSec/(3600*24)) > 0 ? Math.floor(totalSec/(3600*24)) : 0;
+    const remainingHours = Math.floor((totalSec/3600)%24) > 0 ? Math.floor((totalSec/3600)%24) : 0;
+    const remainingMinutes = Math.floor((totalSec/60)%60) > 0 ? Math.floor((totalSec/60)%60) : 0;
+    const remainingSecs = Math.floor(totalSec%60) >= 0 ? Math.floor(totalSec%60) : 0;
+    
+    days.innerHTML = remainingDays;
+    hours.innerHTML = remainingHours;
+    mins.innerHTML = remainingMinutes;
+    secs.innerHTML = remainingSecs;
+}
+timer();
+setInterval(timer,1000);
